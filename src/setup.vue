d98bfc2e0dbfaef13b36c0fbe64855a5f36ffbb3
sjouk jonker <sjoukjonker13@gmail.com>
2024-03-08 14:30:11 +0100

    update

---
+++ src/setup.vue
@@ -0,0 +1,21 @@
+<template>
+    <canvas id="webgl"></canvas>
+</template>
+
+<script>
+import  from '../Experience/Experience';
+
+export default {
+    setup() {
+
+    },
+    mounted() {
+        const experience = new Experience(document.querySelector('canvas#webgl'));
+        console.log('configurator loaded')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
