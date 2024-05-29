f57154349d67bfba34153165e69bdd57e0801a65
sjouk jonker <sjoukjonker13@gmail.com>
2024-05-29 15:21:26 +0200

    driehoek

--- src/threemain/objecten/driehoek.js
+++ src/threemain/objecten/driehoek.js
@@ -1,29 +1,86 @@
 import * as THREE from 'three';
-
+import { useMenuStore } from "../../server/menustore";
 
 export class Driehoek {
     constructor(scene){
-        let geometrie = new THREE.BufferGeometry();
-        let  vertices = new Float32Array([
-            -0.1, 0, 0,  // Vertex 1
-            -0.1, 1.4, 0,  // Vertex 2
-            -0.1, 1.4, 1,   // Vertex 3
 
-            // apartheid
-            0, 0, 0,  // Vertex 1
-            0, 1.4, 0,  // Vertex 2
-            0, 1.4, 1,   // Vertex 3
-          
+        this.store = useMenuStore();
+
+
+        
+        this.geometrie = new THREE.BufferGeometry();
+        this.scaleFactor = 1000
+        this.hoogte = (this.store.hoogte/ this.scaleFactor)  + 0.4;
+      
+        this.vertices = new Float32Array([
+            // links driehoek buitekant
+            -0.1, 0, 0,  // Vertex 1
+            -0.1, this.hoogte, 0,  // Vertex 2
+            -0.1, this.hoogte, 1,   // Vertex 3
+
+            // rechter buiten kant
+            0.1, 0, 0,  // Vertex 4
+            0.1, this.hoogte, 0,  // Vertex 5
+            0.1, this.hoogte, 1,   // Vertex 6
+
+            
+            // links boven driehoek
+            -0.1, this.hoogte, 0,  // Vertex 4
+            -0.1, this.hoogte, 1,  // Vertex 5
+            0.1, this.hoogte, 1,   // Vertex 6
+
+            // rechts boven driehoek
+            -0.1, this.hoogte, 0,  // Vertex 7
+            0.1, this.hoogte, 0,  // Vertex 8
+            0.1, this.hoogte, 1,   // Vertex 9
+
+
+            //schuin droe hoek 1 
+            0.1, 0, 0,  // Vertex 10
+            0.1, this.hoogte, 0,  // Vertex 11
+            -0.1, this.hoogte, 1,   // Vertex 12
+
+            
+            // schuin driehoek 2
+            0.1, 0, 0,  // Vertex 11
+            -0.1, 0, 0,  // Vertex 10
+            -0.1, this.hoogte, 1,   // Vertex 12
         ]);
 
         // Maak een nieuw attribuut voor de vertices en voeg het toe aan de geometrie
-        geometrie.setAttribute('position', new THREE.BufferAttribute(vertices, 3,  true));
-        geometrie.scale(1,1,1)
+        this.geometrie.setAttribute('position', new THREE.BufferAttribute(this.vertices, 3,  true));
 
-        let materiaal = new THREE.MeshBasicMaterial({color: "black", side: THREE.DoubleSide });
-        this.balkenDriehoek = new THREE.Mesh(geometrie, materiaal);
+
+
+
+        this.geometrie.scale(1,1,1)
+
+        this.meshMateriaal = new THREE.MeshStandardMaterial({ color:0x9acd32,  side: THREE.DoubleSide });
+        this.balkenDriehoek = new THREE.Mesh(this.geometrie, this.meshMateriaal);
+
+        // Verbind de overeenkomstige punten van de twee driehoeken
+   
+
+
+        // Maak een groep en voeg de driehoeken en de verbindingslijn toe
+        this.groep = new THREE.Group();
+        this.groep.add(this.balkenDriehoek);
+      
+
+        // Voeg de groep toe aan de scene
+        scene.add(this.groep);
+    }
+
+
+
+
+    gradenCalculatie(){
+        this.graden = this.store.graden;
+
+  
         
-         
-        scene.add(this.balkenDriehoek)
+
+        
+
     }
 }
