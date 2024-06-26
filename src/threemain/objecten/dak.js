37966902c7d5f8a3cec117cf2e1f674dd919b711
sjouk jonker <sjoukjonker13@gmail.com>
2024-06-26 10:15:12 +0200

    s

--- src/threemain/objecten/dak.js
+++ src/threemain/objecten/dak.js
@@ -38,16 +38,16 @@ export class Roof {
     // scalefactor om te delen
     this.scaleFactor = 1000;
     // locatie voorkant
-    this.voorkant.position.y = 0.9;
+
     this.voorkant.position.z = -0.4;
     // locatie zijkant
-    this.zijkant.position.y = 0.9;
+   
     this.zijkant.position.x = 1.4;
-    this.zijkant.position.z = 0.6;
+    
 
     // locatie balk rechts
-    this.cubezijkantrechts.position.y = 0.9;
-    this.cubezijkantrechts.position.z = 0.6;
+   
+
     this.cubezijkantrechts.position.x = -1.4
     // group add
     this.group.add(this.dak, this.zijkant, this.voorkant,  this.cubezijkantrechts);
@@ -67,20 +67,27 @@ export class Roof {
     this.dak.position.y = hoogte / 2 + 0.35;
 
     //z is lengte
-    this.dak.scale.z = lengte + 0.4;
+    this.dak.scale.z = lengte + 0.5;
     this.dak.position.z = lengte / 2 - 0.15;
   }
   addCubeVoorKant() {
     this.breedte = this.store.breedte / this.scaleFactor + 1.6;
+    this.hoogte = this.store.hoogte / this.scaleFactor
+    this.voorkant.position.y = (this.hoogte / 2 )+ 0.40;
    this.voorkant.scale.set(this.breedte, 1, 1) 
   }
 
   zijkantscaling() {
     this.lengte = this.store.lengte / this.scaleFactor;
     this.breedte = this.store.breedte / this.scaleFactor;
-  
+    this.hoogte = this.store.hoogte / this.scaleFactor
     this.zijkant.scale.z = this.lengte + 0.7;
     this.cubezijkantrechts.scale.z = this.lengte + 0.7;
+    this.zijkant.position.y =( this.hoogte /2) +0.40;
+    this.cubezijkantrechts.position.y = (this.hoogte /2 )+0.40;
+    const zAdjustment = this.lengte / 2 -0.1;// Adjust this factor as needed
+    this.zijkant.position.z = +(zAdjustment); // Move slightly backward as object grows
+    this.cubezijkantrechts.position.z = +(zAdjustment); // Same for the opposite side
   
     let richtingZijkant;
     if (this.zijkant.position.x > 0) {
